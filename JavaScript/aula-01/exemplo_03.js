d5cdefb72fa139320348bf34a63a675be5f8017a
PEDRO VERMÖHLER SANTIAGO <pedro_v_santiago@estudante.sesisenai.org.br>
2026-07-06 17:06:21 -0300

    UPDATE 67

---
+++ JavaScript/aula-01/exemplo_03.js
@@ -0,0 +1,5 @@
+console.log("ola mundo")
+let nome = "asper"
+const ano = "1945"
+
+console.log(nome, ano)
---
+++ JavaScript/aula-01/exemplo_04.js
@@ -0,0 +1,5 @@
+let nome = "asper";
+let idade = 15;
+let cidade = "xique-xique, bahia"
+
+console.log("me chamo ", nome, ", tenho ", 15,"")
